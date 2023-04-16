0820e1087dfb96a2281bc4153058b741dce70c6f
William Schutte <williamdschutte@gmail.com>
2023-04-15 21:33:59 -0400

    Address theme file in pages directory to allow Next to build

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,7 @@
 import "@/styles/globals.css";
 import { ChakraProvider, extendTheme } from "@chakra-ui/react";
 
-import { theme } from "./theme";
+import { theme } from "../styles/theme";
 
 export default function App({ Component, pageProps }) {
   return (
